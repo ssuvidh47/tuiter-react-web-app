bcd740bd81949c3bbb1e31d678ea5be60fbef2a6
Suvidh <ssuvidh47@gmail.com>
2023-08-09 15:29:55 -0400

    a5 assignment

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
-import { FaRegComment, FaRetweet, FaHeart, FaShare } from 'react-icons/fa';
+import { FaRegComment, FaRetweet, FaHeart, FaShare,FaThumbsDown } from 'react-icons/fa';
+import { updateTuitThunk } from "../services/tuits-thunks";
+import { useDispatch } from "react-redux";
+
+
+
 
 function TuitStats({ tuit }) {
+  const dispatch = useDispatch();
   const pinkColor = '#FF69B4';
   const [likes, setLikes] = useState(tuit.likes);
   const [isLiked, setIsLiked] = useState(tuit.liked);
+  const [dislikes, setDislikes] = useState(tuit.dislikes);
+  const [isDisliked, setIsDisliked] = useState(tuit.disliked);
 
   const handleLikeClick = () => {
     const updatedLikes = isLiked ? likes - 1 : likes + 1;
@@ -12,27 +20,39 @@ function TuitStats({ tuit }) {
     setIsLiked(!isLiked);
   };
 
+  const handleDislikeClick = () => {
+    const updatedDislikes = isDisliked ? dislikes - 1 : dislikes + 1;
+    setDislikes(updatedDislikes);
+    setIsDisliked(!isDisliked);
+  };
+
   return (
+
     <div className="row">
-      <div className="col-3">
+      <div className="col-2">
         <span>
           <FaRegComment size={20} color="black" /> {" "}
           {tuit.replies}
         </span>
       </div>
 
-      <div className="col-3">
+      <div className="col-2">
         <span>
           <FaRetweet size={25} color="black" /> {" "}
           {tuit.retuits}
         </span>
       </div>
 
-      <div className="col-3">
+      <div className="col-2">
         <span>
+
           <button
             style={{ background: 'none', border: 'none', cursor: 'pointer', borderColor: isLiked ? '#e0245e' : 'transparent' }}
-            onClick={handleLikeClick}
+            onClick={() => {
+              dispatch(updateTuitThunk({ ...tuit, likes: tuit.likes + 1 }));
+              handleLikeClick()
+            }
+            }
           >
             <FaHeart size={20} color={isLiked ? pinkColor : 'black'} />
           </button>
@@ -40,7 +60,19 @@ function TuitStats({ tuit }) {
         </span>
       </div>
 
-      <div className="col-3">
+      <div className="col-2">
+        <span>
+          <button
+            style={{ background: 'none', border: 'none', cursor: 'pointer', borderColor: isDisliked ? '#000000' : 'transparent' }}
+            onClick={() => handleDislikeClick()}
+          >
+            <FaThumbsDown size={20} color={isDisliked ? '#000000' : 'black'} />
+          </button>
+          {dislikes}
+        </span>
+      </div>
+
+      <div className="col-1">
         <span>
           <FaShare size={25} />
         </span>
